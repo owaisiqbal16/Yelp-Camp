f8b32f42c75c6e4967e60d5bddc25384ba13e85f
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-11 23:08:38 +0500

    Campgrounds are associated with specific user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,17 +19,21 @@ router.get("/", function (req, res) {
 })
 
 // NEW ROUTE - Show form to create new campground
-router.get("/new", function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
     res.render("campgrounds/new.ejs");
 })
 
 //CREATE ROUTE - Add new campground to database
-router.post("/", function (req, res) {
+router.post("/",isLoggedIn, function (req, res) {
     //Get data from form and add to the db
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = { name: name, image: image, description: desc };
+    var author = {
+        id: req.user._id ,
+        username: req.user.username
+    }
+    var newCampground = { name: name, image: image, description: desc , author : author};
     //Create a new campground and save to dbs
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
@@ -57,5 +61,12 @@ router.get("/:id", function (req, res) {
         }
     })
 })
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
