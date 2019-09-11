bb4d8b25d1311473786eaf54820096ed37864193
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-12 00:08:20 +0500

    Delete functionality added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -89,6 +89,18 @@ router.put("/:id" , function(req,res){
     //redirect to somewhere(show page)
 })
 
+//DESTROY CAMPGROUND
+router.delete("/:id" , function(req,res){
+    Campground.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        }
+        else{
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
